2a50b13c64d70f0ca7817c163881e993273fa3dc
raiqamar769 <32240796+raiqamar769@users.noreply.github.com>
2017-11-06 20:29:06 +0800

    upload data

---
+++ water_level.ino
@@ -0,0 +1,53 @@
+#define trigPin 12
+#define echoPin 11
+#define buzzer 3
+#define led2 6
+
+int sound = 0;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(led2, OUTPUT);
+ 
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+ 
+
+  if (distance <= 5) {
+    digitalWrite(led2, HIGH);
+    sound = 400;
+}else {
+       sound = 0;
+      digitalWrite(led2, LOW);
+  }
+
+
+  
+  
+ 
+  if (distance > 30){
+    Serial.println("Out of range");
+    noTone(buzzer);
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+    tone(buzzer, sound);
+   
+  }
+  delay(500);
+}
+
